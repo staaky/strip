1a3a3dd4e2418bbb9d1962b65fae85a1dc2183eb
Nick Stakenburg <nick@nickstakenburg.com>
2015-04-08 02:04:48 +0200

    Fixed viewport calculation for Firefox on Android

--- src/js/helpers/bounds.js
+++ src/js/helpers/bounds.js
@@ -1,14 +1,17 @@
 var Bounds = {
   viewport: function() {
     var dimensions = {
-      height: $(window).height(),
       width:  $(window).width()
     };
 
     // Mobile Safari has a bugged viewport height after scrolling
-    if (Browser.MobileSafari) {
+    // Firefox on Android also has problems with height
+    if (Browser.MobileSafari || (Browser.Android && Browser.Gecko)) {
       var zoom = document.documentElement.clientWidth / window.innerWidth;
       dimensions.height = window.innerHeight * zoom;
+    } else {
+      // default
+      dimensions.height = $(window).height();
     }
 
     return dimensions;
